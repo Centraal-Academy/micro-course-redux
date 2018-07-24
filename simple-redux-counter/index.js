adbbfaac947a75142e90ec022b4587b2512a95b2
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-23 20:43:35 -0500

    redux counter

---
+++ simple-redux-counter/index.js
@@ -0,0 +1,43 @@
+/* global Redux */
+const CALCULATOR_ADD = 'CALCULATOR_ADD'
+const CALCULATOR_SUBSTRACT = 'CALCULATOR_SUBSTRACT'
+
+function add (value) {
+  return {
+    type: CALCULATOR_ADD,
+    value
+  }
+}
+
+function substract (value) {
+  return {
+    type: CALCULATOR_SUBSTRACT,
+    value
+  }
+}
+
+function rootReducer (state = 0, action) {
+  switch (action.type) {
+    case CALCULATOR_ADD:
+      return state + action.value
+    case CALCULATOR_SUBSTRACT:
+      return state - action.value
+    default:
+      return state
+  }
+}
+
+const store = Redux.createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+store.subscribe(() => {
+  document.getElementById('result').innerText = store.getState()
+})
+
+document.getElementById('add').addEventListener('click', (e) => {
+  e.preventDefault()
+  store.dispatch(add(1))
+})
+document.getElementById('substract').addEventListener('click', (e) => {
+  e.preventDefault()
+  store.dispatch(substract(1))
+})
