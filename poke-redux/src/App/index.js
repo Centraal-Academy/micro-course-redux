18dba6b9ef1186aecdc0cb2760849f72de402b04
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-25 18:42:57 -0500

    Application component

---
+++ poke-redux/src/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Pokedex from '../components/Pokedex'
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <Pokedex />
+    )
+  }
+}
