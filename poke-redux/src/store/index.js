b043044e15b4806f554f7cc28a29cf264d8d9ca4
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-26 17:44:46 -0500

    Added saga middleware

--- poke-redux/src/store/index.js
+++ poke-redux/src/store/index.js
@@ -1,9 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
+// import thunk from 'redux-thunk'
 import { rootReducer } from '../reducer'
+import sagaMiddlewareCreator from 'redux-saga'
+import rootSaga from '../sagas'
+
+const sagaMiddleware = sagaMiddlewareCreator()
 
 export default createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk)
+  applyMiddleware(sagaMiddleware)
 )
+
+sagaMiddleware.run(rootSaga)
