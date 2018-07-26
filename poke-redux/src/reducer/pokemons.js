5c4bcdebae303676444e68e0764464a1a8c19fad
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-26 03:01:01 -0500

    added reducer to capture pokemon

--- poke-redux/src/reducer/pokemons.js
+++ poke-redux/src/reducer/pokemons.js
@@ -1,11 +1,15 @@
-import { ADD_POKEMONS, LIKE_POKEMON } from '../actions/types'
+import { ADD_POKEMONS, LIKE_POKEMON, CAPTURE_POKEMON } from '../actions/types'
 
 function addPokemonsReducer (state = {}, action) {
   return { ...state, ...action.payload.pokemons }
 }
 
 function likePokemonReducer (state = {}, action) {
-  return { ...state, like: !state.pokemon.like }
+  return { ...state, like: !state.like }
+}
+
+function capturePokemonReducer (state = {}, action) {
+  return { ...state, captured: true }
 }
 
 export default function PokemonsReducer (state = {}, action) {
@@ -13,8 +17,11 @@ export default function PokemonsReducer (state = {}, action) {
     case ADD_POKEMONS:
       return addPokemonsReducer(state, action)
     case LIKE_POKEMON:
-      const { pokemon } = action
-      return likePokemonReducer(state[pokemon.id], action)
+      const { pokemon } = action.payload
+      return { ...state, [pokemon.id]: likePokemonReducer(state[pokemon.id], action) }
+    case CAPTURE_POKEMON:
+      const pokemonCaptured = action.payload.pokemon
+      return { ...state, [pokemonCaptured.id]: capturePokemonReducer(state[pokemonCaptured.id], action) }
     default:
       return state
   }
