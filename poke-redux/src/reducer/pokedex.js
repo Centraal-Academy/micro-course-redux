c179741fc42adf13075ed4eadd640c523d60d6d8
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-26 02:59:47 -0500

    added reducer loading to pokedex reducer

--- poke-redux/src/reducer/pokedex.js
+++ poke-redux/src/reducer/pokedex.js
@@ -1,9 +1,11 @@
 import PokemonsReducer from './pokemons'
 import SeachReducer from './search'
+import loadingPokedexReducer from './loadingPokedex'
 
 export default function (state = {}, action) {
   return {
     pokemons: PokemonsReducer(state.pokemons, action),
-    search: SeachReducer(state.search, action)
+    search: SeachReducer(state.search, action),
+    loading: loadingPokedexReducer(state.loading, action)
   }
 }
