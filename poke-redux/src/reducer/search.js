7c6889e1e49c161d367952f1b43da2aac5bc5ace
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-26 03:00:21 -0500

    fixed reducer

--- poke-redux/src/reducer/search.js
+++ poke-redux/src/reducer/search.js
@@ -2,7 +2,7 @@ import { SEARCH_POKEMON } from '../actions/types'
 
 function searchReducer (state = '', action) {
   if (action.type === SEARCH_POKEMON) {
-    return action.payload.search
+    return action.payload.query
   }
   return state
 }
