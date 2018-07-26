28a538e05b421ecaf4b76a2bf5a5c5cbae28265d
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-26 17:43:35 -0500

    rootsaga and fetchpokemonssaga

---
+++ poke-redux/src/sagas/index.js
@@ -0,0 +1,16 @@
+/* global fetch */
+import { FETCH_POKEMON } from '../actions/types'
+import { loadingPokedexPokemons, addPokemons } from '../actions'
+import { call, put, takeEvery } from 'redux-saga/effects'
+
+export function * fetchPokemonsSaga () {
+  yield put(loadingPokedexPokemons(true))
+  const response = yield call(fetch, 'http://localhost:3000/pokemons')
+  const pokemons = yield call([response, response.json])
+  yield put(loadingPokedexPokemons(false))
+  yield put(addPokemons(pokemons))
+}
+
+export default function * rootSaga () {
+  yield takeEvery(FETCH_POKEMON, fetchPokemonsSaga)
+}
