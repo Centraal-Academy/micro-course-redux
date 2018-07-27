636a348d0793a6649c7256500fed508cd2cd0221
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-27 15:34:54 -0500

    refactored component

--- poke-redux/src/components/Pokedex/PokeList.js
+++ poke-redux/src/components/Pokedex/PokeList.js
@@ -1,24 +1,13 @@
 import React from 'react'
 import PokeListItem from './PokeListItem'
-function makeFilter (query) {
-  return function filter (pokemon) {
-    return pokemon.name.toLowerCase().includes(query.toLowerCase())
-  }
-}
 
 function PokeList (props) {
-  let pokemons
-  if (props.filter) {
-    const filter = makeFilter(props.filter)
-    pokemons = props.pokemons.filter(filter)
-  } else {
-    pokemons = props.pokemons
-  }
   return (
     <ul>
       {
-        pokemons.map(
-          pokemon => <PokeListItem key={pokemon.id} pokemon={pokemon} onClick={props.onClick} />
+        props.pokemons.map(
+          pokemon =>
+            <PokeListItem key={pokemon.id} pokemon={pokemon} onClick={props.onClick} />
         )
       }
     </ul>
